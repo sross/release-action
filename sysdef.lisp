961db07df5e7ab6a9958212ac3e049b4ea3896e4
Sean Ross <sross@hacktar.local>
2008-10-22 22:01:01 +0100

    bump version

--- sysdef.lisp
+++ sysdef.lisp
@@ -1,6 +1,6 @@
 (in-package #:sysdef-user)
 
 (define-system :release-action ()
- (:version 0 1 1)
+ (:version 0 1 2)
  (:components "releaser")
  (:requires :archive :gzip-stream (:mb.sysdef :version (>= 0 1 9))))
